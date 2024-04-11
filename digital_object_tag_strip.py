1758875d360a54a998af7974c0be6db32d6e5d34
Channing Norton <24469058+C-Norton@users.noreply.github.com>
2024-04-11 15:28:28 -0400

    reformatted digital_object_tag_strip.py using black

--- digital_object_tag_strip.py
+++ digital_object_tag_strip.py
@@ -18,11 +18,13 @@ def setup():
     logFile = input("Please enter a CSV filename for log output: ")
     if not os.path.isfile(logFile):
         if not os.path.exists(logFile):
-            log = open(logFile, 'a+')
+            log = open(logFile, "a+")
             log.write("Date/Time,URI,Previous Value, Updated Value, Status")
             log.close()
         else:
-            print(f"Cannot write to {logFile}: Is a directory. ArchivesSpace has not been modified")
+            print(
+                f"Cannot write to {logFile}: Is a directory. ArchivesSpace has not been modified"
+            )
             return
     else:
         print("Warning: Logfile already exists, log output will be appended")
@@ -31,9 +33,10 @@ def setup():
 
 
 def log(uri, previousvalues, updatedvalues, result):
-    file = open(logFile, 'a+')
+    file = open(logFile, "a+")
     file.write(
-        f"\n{datetime.datetime.now()},{uri},{previousvalues},{updatedvalues},{'Success' if result == 200 else result}")
+        f"\n{datetime.datetime.now()},{uri},{previousvalues},{updatedvalues},{'Success' if result == 200 else result}"
+    )
     file.close()
 
 
@@ -50,12 +53,18 @@ def create_session(file="ArchivesSpaceConnection.txt"):
 def strip_tags(session, reponum, resourcenum):
     maxpage = 1
     page = 1
-    while (page <= maxpage):
+    while page <= maxpage:
         page += 1
-        result = session.get(f"repositories/{reponum}/digital_objects", params={"page": page}).json()
+        result = session.get(
+            f"repositories/{reponum}/digital_objects", params={"page": page}
+        ).json()
         maxpage = result["last_page"]
         for result in result["results"]:
-            if resourcenum == 0 or {"ref": f"/repositories/{reponum}/resources/{resourcenum}"} in result["collection"]:
+            if (
+                resourcenum == 0
+                or {"ref": f"/repositories/{reponum}/resources/{resourcenum}"}
+                in result["collection"]
+            ):
                 process_digital_object(session, result)
 
 
@@ -63,16 +72,22 @@ def process_digital_object(session, result: json):
     edited = copy.deepcopy(result)
     wasChanged = False
     for versionnum in range(len(result["file_versions"])):
-        preservicalink = re.findall('<a href="(https://uorrcl.access.preservica.com/.+?/)">',
-                                    result["file_versions"][versionnum]["file_uri"])
+        preservicalink = re.findall(
+            '<a href="(https://uorrcl.access.preservica.com/.+?/)">',
+            result["file_versions"][versionnum]["file_uri"],
+        )
         if preservicalink:
             edited["file_versions"][versionnum]["file_uri"] = preservicalink[0]
             wasChanged = True
 
     if wasChanged:
         request = session.post(f"{result['uri']}", json=edited)
-        log(result["uri"], result["file_versions"][1]["file_uri"], edited["file_versions"][1]["file_uri"],
-            request.json()["status"])
+        log(
+            result["uri"],
+            result["file_versions"][1]["file_uri"],
+            edited["file_versions"][1]["file_uri"],
+            request.json()["status"],
+        )
 
 
 setup()
